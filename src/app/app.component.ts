aa3f7f40e9502ea3edafdf73927c25647b4e63d2
Marcelo Vergara <marcelorv@gmail.com>
2023-10-29 10:21:33 -0300

    Types for requests and responses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +20,7 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
-      .post(
+      .post<{name: string}>(
         'https://fqm-dsv-default-rtdb.firebaseio.com/posts.json',
         postData)
       .subscribe(responseData => {
@@ -38,9 +39,9 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http
-    .get('https://fqm-dsv-default-rtdb.firebaseio.com/posts.json')
+    .get<{[key: string]: Post}>('https://fqm-dsv-default-rtdb.firebaseio.com/posts.json')
     .pipe(map(responseData => {
-      const postsArray = []
+      const postsArray: Post[] = []
       for (const key in responseData) {
         if (responseData.hasOwnProperty(key)) {
           postsArray.push({ ...responseData[key], id: key})}
