518d3dc8a7220a038e249ec078c366c290e27165
Marcelo Vergara <marcelorv@gmail.com>
2023-11-01 13:18:58 -0300

    Interceptor

---
+++ src/app/auth-interceptor.service.ts
@@ -0,0 +1,9 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+
+export class AuthInterceptorService implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    console.log(req, 'is on its way')
+    return next.handle(req)
+  }
+}
