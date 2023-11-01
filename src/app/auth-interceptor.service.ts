f56572cdb5186192b2fb826307de4d8ee8292a40
Marcelo Vergara <marcelorv@gmail.com>
2023-11-01 13:30:45 -0300

    Multiple interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -5,13 +5,8 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // if(req.url)
-    console.log('Req is on its way', req.url)
+    // console.log('Req is on its way', req.url)
     const modifiedRequest = req.clone({headers: req.headers.append('auth', 'bearer szxv')})
-    return next.handle(modifiedRequest).pipe(tap(event => {
-      console.log('event', event)
-      if (event.type === HttpEventType.Response) {
-        console.log('Response arrived, body data', event.body)
-      }
-    }))
+    return next.handle(modifiedRequest)
   }
 }
